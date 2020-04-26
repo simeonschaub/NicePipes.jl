9b2fbdb09189a39803d575a8b4d835c74cbdb858
zeptodoctor <44736852+zeptodoctor@users.noreply.github.com>
2020-04-26 00:07:38 +0000

    build based on ca13311

---
+++ v0.1.1/search_index.js
@@ -0,0 +1,3 @@
+var documenterSearchIndex = {"docs":
+[{"location":"#","page":"Home","title":"Home","text":"CurrentModule = NicePipes","category":"page"},{"location":"#NicePipes-1","page":"Home","title":"NicePipes","text":"","category":"section"},{"location":"#","page":"Home","title":"Home","text":"","category":"page"},{"location":"#","page":"Home","title":"Home","text":"Modules = [NicePipes]","category":"page"}]
+}
